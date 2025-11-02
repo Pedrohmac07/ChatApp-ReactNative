eba848b9f42f12f4f2f7714bbd95b06a59ab606a
Pedro Machado <ph374011@gmail.com>
2025-11-02 11:41:42 -0300

    Added constant and utils

---
+++ types.ts
@@ -0,0 +1,66 @@
+import { scale, verticalScale } from "@/utils/styling";
+
+export const colors = {
+  "background": "#121212",
+  "primary": "#1A237E",
+  "primaryDark": "#000051",
+  "secondaryHighlight": "#4DD0E1",
+  "button": "#4DD0E1",
+  "buttonPressed": "#00AEC9",
+  "textLight": "#FFFFFF",
+  "textSecondary": "#BDBDBD",
+  "bubbleUser": "#4DD0E1",
+  "bubbleContact": "#37474F",
+  "textBubbleUser": "#121212",
+  "textBubbleContact": "#FFFFFF"
+}
+
+export const spacingX = {
+  _3: scale(3),
+  _5: scale(5),
+  _7: scale(7),
+  _10: scale(10),
+  _12: scale(12),
+  _15: scale(15),
+  _17: scale(17),
+  _20: scale(20),
+  _25: scale(25),
+  _30: scale(30),
+  _35: scale(35),
+  _40: scale(40),
+}
+
+export const spacingY = {
+  _3: scale(3),
+  _5: scale(5),
+  _7: scale(7),
+  _10: scale(10),
+  _12: scale(12),
+  _15: scale(15),
+  _17: scale(17),
+  _20: scale(20),
+  _25: scale(25),
+  _30: scale(30),
+  _35: scale(35),
+  _40: scale(40),
+  _50: scale(50),
+  _60: scale(60),
+}
+
+export const radius = {
+  _3: verticalScale(3),
+  _6: verticalScale(6),
+  _10: verticalScale(10),
+  _12: verticalScale(12),
+  _15: verticalScale(15),
+  _17: verticalScale(17),
+  _20: verticalScale(20),
+  _30: verticalScale(30),
+  _40: verticalScale(40),
+  _50: verticalScale(50),
+  _60: verticalScale(60),
+  _70: verticalScale(70),
+  _80: verticalScale(80),
+  _90: verticalScale(90),
+  full: 200,
+}
